7bf66d203c27f87b2b5c41f331eb5c6df2308005
mohammed <mohyus15@hotmail.com>
2022-10-13 14:29:11 +0200

    compile doesn,t run

---
+++ src/test/java/no/kristania/ServerLibrary.java
@@ -0,0 +1,13 @@
+package no.kristania;
+
+import java.net.URL;
+
+public class ServerLibrary {
+    public  ServerLibrary(int port) {
+
+    }
+
+    public URL getURL() {
+        return null;
+    }
+}
---
+++ src/test/java/no/kristania/libraryServerTest.java
@@ -0,0 +1,23 @@
+package no.kristania;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class libraryServerTest {
+    @Test
+
+    void shouldServerHomePage() throws IOException {
+        var server = new ServerLibrary(0);
+     var con =(HttpURLConnection) server.getURL().openConnection();
+     assertThat(con.getResponseCode())
+             .as(con.getResponseMessage() + " for " + con.getURL())
+             .isEqualTo(200);
+        assertThat(con.getErrorStream())
+                .asString(StandardCharsets.UTF_8)
+                .contains("<h1>kristinaia libray</h1>");
+    }
+}
