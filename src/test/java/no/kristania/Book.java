32c07ede2e31b6e1466c5d12f8de4bdbfdbea6b8
mohammed <mohyus15@hotmail.com>
2022-10-16 12:07:15 +0200

    i have created class book with servelet projetct runingn

---
+++ src/test/java/no/kristania/Book.java
@@ -0,0 +1,17 @@
+package no.kristania;
+
+public class Book {
+
+    private String title;
+    private String author;
+
+    public void setTite(String title) {
+        this.title = title;
+
+    }
+
+    public void setAuthor(String author) {
+        this.author= author;
+
+    }
+}
--- src/test/java/no/kristania/ListBooksServerlet.java
+++ src/test/java/no/kristania/ListBooksServerlet.java
@@ -8,11 +8,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.servlet.Source;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ListBooksServerlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+        var book = new Book();
+        book.setTite("java in a nutshell");
+        book.setAuthor("mohammed");
+        var books = List.of(book);
         resp.getWriter().println("java in a nutshell");
     }
 }
